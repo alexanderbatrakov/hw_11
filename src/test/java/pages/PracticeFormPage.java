80a07e547016c3131d72d8430919648171c55fba
Alexander Batrakov <a.batrakov1@gmail.com>
2022-12-10 19:36:18 +0400

    Initial commit

---
+++ src/test/java/pages/PracticeFormPage.java
@@ -0,0 +1,121 @@
+package pages;
+
+import com.codeborne.selenide.selector.ByText;
+import io.qameta.allure.Step;
+import pages.components.CalendarComponent;
+import pages.components.CheckResult;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class PracticeFormPage {
+
+    CalendarComponent calendarComponent = new CalendarComponent();
+    CheckResult checkResult = new CheckResult();
+
+    @Step("Open registration form")
+    public PracticeFormPage openPage() {
+        open("/automation-practice-form");
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+
+        return this;
+    }
+    @Step("Filling registration form")
+    public PracticeFormPage setFirstName(String value) {
+        $("#firstName").setValue(value);
+
+        return this;
+    }
+
+    public PracticeFormPage setLastName(String value) {
+        $("#lastName").setValue(value);
+
+        return this;
+    }
+
+    public PracticeFormPage setUserEmail(String value) {
+        $("#userEmail").setValue(value);
+
+        return this;
+    }
+
+    public PracticeFormPage setAddress(String value) {
+        $("#currentAddress").setValue(value);
+
+        return this;
+    }
+
+    public PracticeFormPage setUserNumber(String value) {
+        $("#userNumber").setValue(value);
+
+        return this;
+    }
+
+    public PracticeFormPage selectGender(String value) {
+        $("#genterWrapper").$(new ByText(value)).click();
+
+        return this;
+    }
+
+    public PracticeFormPage selectDateOfBirth(String day, String month, String year) {
+        $("#dateOfBirthInput").click();
+        calendarComponent.setDate(day, month, year);
+
+        return this;
+    }
+
+    public PracticeFormPage setSubject(String subject) {
+        $("#subjectsInput").setValue(subject).pressEnter();
+
+        return this;
+    }
+
+    public PracticeFormPage setHobbies(String hobby) {
+        $("#hobbiesWrapper").$(new ByText(hobby)).click();
+
+        return this;
+    }
+
+    public PracticeFormPage uploadFile(String path) {
+        $("#uploadPicture").uploadFile(new File(path));
+
+        return this;
+    }
+
+    public PracticeFormPage setState(String state) {
+        $("#state").click();
+        $("#stateCity-wrapper").$(new ByText(state)).click();
+
+        return this;
+    }
+
+    public PracticeFormPage setCity(String city) {
+        $("#city").click();
+        $("#stateCity-wrapper").$(new ByText(city)).click();
+
+        return this;
+    }
+
+    public PracticeFormPage clickOnSubmit() {
+        $("#submit").click();
+
+        return this;
+    }
+@Step("Verify result")
+    public PracticeFormPage checkTitle() {
+        checkResult.visibleOfTitle();
+
+        return this;
+    }
+
+    public PracticeFormPage checkContent(String key, String value) {
+        checkResult.checkContentAtTable(key, value);
+
+        return this;
+    }
+
+
+}
---
+++ src/test/java/tests/PracticeFormPageTest.java
@@ -0,0 +1,39 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static tests.TestData.*;
+
+public class PracticeFormPageTest extends TestBase {
+
+    @Test
+    void fillFormTest() {
+        practiceFormPage.openPage()
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setUserEmail(userEmail)
+                .setAddress(currentAddress)
+                .setUserNumber(userNumber)
+                .selectGender(gender)
+                .selectDateOfBirth(dayOfBirth, monthOfBirth, yearOfBirth)
+                .setSubject(subject)
+                .setHobbies(hobby)
+                .uploadFile(picPath)
+                .setState(state)
+                .setCity(city)
+                .clickOnSubmit();
+
+        practiceFormPage.checkTitle()
+                .checkContent("Student Name", firstName + " " + lastName)
+                .checkContent("Student Email", userEmail)
+                .checkContent("Gender", gender)
+                .checkContent("Mobile", userNumber)
+                .checkContent("Date of Birth", dayOfBirth + " " + monthOfBirth + "," + yearOfBirth)
+                .checkContent("Subjects", subject)
+                .checkContent("Hobbies", hobby)
+                .checkContent("Picture", "testfile.jpg")
+                .checkContent("Address", currentAddress)
+                .checkContent("State and City", state + " " + city);
+
+    }
+}
