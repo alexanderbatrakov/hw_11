d501c1ce4553a88920662148807489eb7e96c9de
Alexander Batrakov <a.batrakov1@gmail.com>
2022-12-10 20:12:18 +0400

    added closing of browser

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -15,7 +15,7 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.holdBrowserOpen = true;
+        Configuration.holdBrowserOpen = false;
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
